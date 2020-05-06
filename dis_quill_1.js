ec8405655e88c09fe192b8988b3c32f3c4cd2ffc
yinglan <yinglan@umich.edu>
2020-05-06 17:09:35 -0400

    removed unneeded commented out code

--- dis_quill_1.js
+++ dis_quill_1.js
@@ -32,9 +32,7 @@ $(document).ready(function(){
 		print_runtime_error();
 		//no notes yet - create new note
 		let old_tot = parseInt(tot_num.total_num);
-		//console.log("old_tot = ", old_tot);
 		if(old_tot === 0){
-			//console.log("no notes to start with");
 			let new_tot = old_tot + 1;
 			chrome.storage.local.set(
 				{"next_id": "1", "total_num": "1", "id_list":["0"]}, function(){
@@ -47,12 +45,10 @@ $(document).ready(function(){
 
 		//has prev saved notes -> new tab 
 		else{
-			//console.log("TODO: handle ext update");
 			chrome.storage.local.get("id_list", function(res_dict){
 				print_runtime_error();
 				//TODO: update note when focused
 				for(id of res_dict.id_list){
-					//console.log(id);
 					//create html for note
 					create_note_with_id(id);
 					create_quill(id);
@@ -78,7 +74,6 @@ function store_note(id, quill){
 	let pos_top = get_pos_top(id);
 	let pos_left = get_pos_left(id);
 	let disp = document.getElementById("editor" + id).style.display;
-	//console.log("STORE");
 	chrome.storage.local.set({[id]:
 		{"content":quill.getContents(), "pos_top": [pos_top], "pos_left": [pos_left], "display": [disp]}}, function(){
 		print_runtime_error();
@@ -101,7 +96,6 @@ function store_notes(){
 	Param id: {string} id of the note
 */
 function create_note_with_id(id){
-	//console.log("create_note with id called");
 	console.assert(typeof(id) === "string");
 	let div_str = "<div class='one_note' id='note" + id + "'></div>";
 	let one_note_div = $(div_str);
@@ -119,10 +113,7 @@ function create_note_with_id(id){
 	one_note_div.append(drag);
 	drag.append(min_button);
 	drag.append(del_button);
-	// one_note_div.append(del_button);
-	// one_note_div.append(min_button);
 	one_note_div.append(editor);
-	//$("#boundary-box").append(one_note_div);
 	$("body").append(one_note_div);
 	document.getElementById("note"+id).style.position = "fixed";
 }
@@ -144,8 +135,6 @@ function create_note_helper(id){
 	chrome.storage.local.set({[id]: 
 		{"content": quill.getContents(), "pos_top": [pos_top], "pos_left": [pos_left]}}, function(){
 		print_runtime_error();
-		//console.log("Note " + id + "created - contents saved");
-		//console.log(quill.getContents());
 	});	
 }
 
@@ -158,10 +147,8 @@ function create_note_helper(id){
 
 function update_note(id, quill){
 	console.assert(typeof(id) === "string");
-	//console.log("update_note called");
 	chrome.storage.local.get(id, function(quill_cont){
 		print_runtime_error();
-		//console.log("quill content: ", quill_cont);
 		quill.setContents(quill_cont[id].content);
 		let note = document.getElementById("note"+id);
 		note.style.top = quill_cont[id]["pos_top"];
@@ -187,14 +174,12 @@ function update_notes(){
 			}
 			//Tab is out of date - need to create new note
 			else{
-				//console.log("update note - id = " + id + "doesn't exist");
 				create_note_with_id(id);
 				create_quill(id);
   				update_note(id, map_id_quill[id]);
 			}
 		}); //end of for each
 		//delete from map_id_quill what has already been deleted
-		//TODO: to be tested
 		for (let id in map_id_quill){
 			if(!id_list_res.id_list.includes(id)){
 				delete_note_helper(id);
@@ -207,13 +192,10 @@ function update_notes(){
 /*
 	called when the user click the create ntoe button
 */
-
 function create_note(){
 	console.log("create_note called");
 	chrome.storage.local.get(["total_num", "next_id", "id_list"], function(res){
 		print_runtime_error();
-		// console.log("res.total_num = " + res.total_num);
-		//console.log("res.next_id = " + res.next_id);	
 		let new_tot = (parseInt(res.total_num) + 1).toString();
 		let new_id = (parseInt(res.next_id) + 1).toString();
 		let new_note_id = res.next_id.toString();
@@ -234,7 +216,6 @@ function create_note(){
 */
 function delete_note_helper(id){
 	delete map_id_quill[id];
-	//console.log("id = " + id);
 	let elt_to_delete = document.querySelector("#note" + id);
 	elt_to_delete.parentNode.removeChild(elt_to_delete);
 }
@@ -245,16 +226,10 @@ function delete_note_helper(id){
 */
 //Note: the way to get id is dependent on the HTML structure of the note
 function delete_note(e){
-	//console.log("delete note clicked");
 	//detect which note id to delete\
-	// console.log(e.target.parentElement);
-	// console.log("e.target.parentElement.id = " + e.target.parentElement.id);
- //    let str_id = e.target.parentElement.childNodes[3].id;
- //    let id = str_id.substring(6);
     let str_id = e.target.parentElement.parentElement.id;
     //Note: somewhat hardcoded
     let id = str_id.substring(4);
-    //console.log("del note id = " + id);
 
     //update related values
     chrome.storage.local.get(["total_num", "id_list"], function(res_tot){
@@ -298,7 +273,6 @@ function get_pos_left(id){
 	param: {event} button click event of the minimization button
 */
 function change_dis(e){
-	//console.log(e.target);
 	let str_id = e.target.parentElement.parentElement.id;
 	//Note: somewhat hardcoded
 	let id = str_id.substring(4);
